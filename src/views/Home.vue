42b17099a5b1f5580d661c30f02fc6cf457ea456
Tyler Epstein <tylerepstein27@gmail.com>
2021-04-21 21:25:27 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    {{ places }}
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      places: [],
+      placeName: "",
+      placeAddress: "",
+      currentPlace: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios
+        .get("/api/places")
+        .then((response) => {
+          this.places = response.data;
+        })
+        .catch((error) => console.log(error.response));
+    },
+    createPlace: function () {
+      var params = {
+        name: this.placeName,
+        address: this.placeAddress,
+      };
+      axios
+        .post("/api/places", params)
+        .then((response) => {
+          console.log("Place added!");
+          this.places.push(response);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      this.currentPlace = place;
+      document.querySelector("#product-details").showModal();
+    },
+    updatePlace: function (place) {
+      var params = {
+        name: place.name,
+        address: place.address,
+      };
+      axios.patch("api/places/" + place.id, params).then((response) => {
+        console.log("Update successful", response.data);
+      });
+    },
   },
 };
 </script>
